c95ef4bce748502284b61cf71442bfba41eb0c0a
RolandoRapali <RolandoRapali@users.noreply.github.com>
2021-04-07 03:32:27 -0300

    Fixing queries

--- java-Alkemy/java-Alkemy/src/main/java/com/example/Alumno/Repositorio/AlumnoRepositorio.java
+++ java-Alkemy/java-Alkemy/src/main/java/com/example/Alumno/Repositorio/AlumnoRepositorio.java
@@ -3,6 +3,7 @@ package com.example.Alumno.Repositorio;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import com.example.Alumno.Modelo.Alumno;
 @Repository
 public interface AlumnoRepositorio extends JpaRepository<Alumno, String> {
 
-	Optional<Alumno> findStudentByDni(String dni);
+	@Query("SELECT a FROM Alumno a WHERE a.dni = ?1")
+	Alumno findStudentByDni(String dni);
 }
--- java-Alkemy/java-Alkemy/src/main/java/com/example/Alumno/Repositorio/MateriaRepositorio.java
+++ java-Alkemy/java-Alkemy/src/main/java/com/example/Alumno/Repositorio/MateriaRepositorio.java
@@ -1,8 +1,10 @@
 package com.example.Alumno.Repositorio;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.Alumno.Modelo.Materia;
@@ -10,5 +12,8 @@ import com.example.Alumno.Modelo.Materia;
 @Repository
 public interface MateriaRepositorio extends JpaRepository<Materia, Integer> {
 
-	//Optional<Materia> findSubjectByName(String name);
+	@Query("SELECT s FROM Materia s WHERE s.id = ?1")
+	Materia findSubjectById(int id);
+	@Query("SELECT s FROM Materia s WHERE s.nombre = ?1")
+	Materia findSubjectByName(String name);
 }
